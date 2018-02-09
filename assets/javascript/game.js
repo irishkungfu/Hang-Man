b99e9e745ef3a2e9abb513544334116f2655d986
Jared Gilpin <irishkungfu@gmail.com>
2018-02-09 11:41:16 -0600

    initial working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,77 @@
-var word = ['triple', 'double', 'strike', 'ball', 'jersey','catch'];
-var i;
-var answerOutput = [];
-var msg = document.getElementById('state-msg');
-var incorrect = 12;
-incorrectGuesses = [];
+var words = ['test', 'triple', 'illusion'];
+var word;
 
-function chooseWord (answer) {
-    return answer[Math.floor((Math.random() * answer.length))]
-}
-function wordLength (wordToMeasure) {
-    return wordToMeasure.length;
-}
-function keyLog(e) {
-    var keyPush = String.fromCharCode(e.keyCode);
-    console.log(keyPush); // Diagnostic test of keyboard input
-    // for (var i = 0; i < wordCharLength; i++) {
-    //     if (keyPush === answerWord[i]) {
-    //         answerOutput[i] = keyPush;  
-    //         console.log(answerOutput);
-    //         msg.textContent = answerOutput;
+guesses = 13;
+incorrect_guesses = [];
+answer = [];
+wins = 0;
 
-    //     } else {
-    //         incorrect++;
-    //         console.log('Incorrect Guesses: ' + incorrect);
-    //     }
-    // }
-    if (answerWord.includes(keyPush)) {
-        var answerIndex = answerWord.indexOf(keyPush);
-        console.log('correct guess');
-    } else {
-        console.log('incorrect guess');
-        if (incorrect !== 0) {
-            incorrect--;
-            console.log("incorrect guesses left: " + incorrect);
-        } else {
-            console.log('sorry, you lost');
+// Create spans for answer to rest in -- each letter gets a span
+
+function createAnswer () {
+    word = words[Math.floor(Math.random() * words.length)];
+    console.log("Selected word is: " +  word); // diag
+    for(var i = 0; i < word.length; i++) {
+        var node = document.createElement("span");                 // Create a <div> node
+        node.className = 'letter';
+        node.id = 'letter_' + i;
+        var textnode = document.createTextNode(" ");         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("answer").appendChild(node);     // Append <li> to <ul> with id="myList"
+    }
+}
+function restartUI () {
+    incorrect_guesses = [];
+    answer = [];
+    guesses = 13;
+    document.getElementById("remaining_guesses").textContent = guesses;
+    document.getElementById('answer').innerHTML = "";
+    document.getElementById("incorrect").textContent = incorrect_guesses;
+    createAnswer()
+}
+
+createAnswer();
+
+document.onkeyup = function(event) {
+  console.log("You pressed: " + event.key);
+    if (word.indexOf(event.key) > -1) {             // correct guess
+        console.log("you matched on a letter");
+        var indices = [];
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === event.key) indices.push(i);
+            // answer[indices[i]] = event.key;
+            // console.log("Answer = " + answer);
+        }
+        console.log("Indices match = " + indices);
+        for (var j = 0; j < indices.length; j++) {
+            document.getElementById("letter_" + indices[j]).textContent = event.key;
+            answer[indices[j]] = event.key;
+            console.log("Answer = " + answer);
+            console.log(answer.join(""));
+            if (answer.join("") === word) {
+                alert('You Win!');
+                wins++;
+                document.getElementById('wins').textContent = wins;
+                restartUI();   // Restart the app   
+            }
         }
     }
-}
-function outputTemplate(length) {
-    for (i = 0; i < wordCharLength; i++) {
-        answerOutput[i] = '';
-        console.log(answerOutput);
+    else {  // incorrect guess
+        if (incorrect_guesses.indexOf(event.key) === -1) {
+            guesses--;
+            incorrect_guesses.push(event.key);
+            document.getElementById("remaining_guesses").textContent = guesses;
+
+            for (var k = 0; k < incorrect_guesses.length; k++) {
+                console.log("Incorrect Guesses: " + incorrect_guesses[k]);
+                document.getElementById("incorrect").textContent = incorrect_guesses;
+            }
+
+        }      
+        if (guesses === 0) {
+            alert ('you lose');
+            restartUI();   // Restart the app
+             
+        }
     }
-}
-
-var answerWord = chooseWord(word);
-var wordCharLength = wordLength(answerWord);
-
-// console.log(answerWord);
-// console.log(wordLength(answerWord));
-
-console.log("answerWord = " + answerWord + " wordCharLength = " + wordCharLength);
-outputTemplate(wordCharLength);
-
-
-document.body.addEventListener('keypress', keyLog);
-
-
+};
