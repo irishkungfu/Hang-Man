e8fd1866db80cb3ce297e3c7706540dff49235c3
Jared Gilpin <irishkungfu@gmail.com>
2018-02-09 15:25:53 -0600

    add styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
-var words = ['test', 'triple', 'illusion'];
+var words = ['gone with the wind'];
 var word;
+var usedWords = []
 
 guesses = 13;
 incorrect_guesses = [];
@@ -9,16 +10,21 @@ wins = 0;
 // Create spans for answer to rest in -- each letter gets a span
 
 function createAnswer () {
-    word = words[Math.floor(Math.random() * words.length)];
-    console.log("Selected word is: " +  word); // diag
-    for(var i = 0; i < word.length; i++) {
-        var node = document.createElement("span");                 // Create a <div> node
-        node.className = 'letter';
-        node.id = 'letter_' + i;
-        var textnode = document.createTextNode(" ");         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
-        document.getElementById("answer").appendChild(node);     // Append <li> to <ul> with id="myList"
-    }
+    if (typeof words !== 'undefined' && words.length > 0) {
+        word = words[Math.floor(Math.random() * words.length)];
+
+        words = words.filter(function(a) {return a !== word});
+        for(var i = 0; i < word.length; i++) {
+            var node = document.createElement("span");                 // Create a <span> node
+            node.className = 'letter';
+            node.id = 'letter_' + i;
+            var textnode = document.createTextNode("-");         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <span>
+            document.getElementById("answer").appendChild(node);     // Append <span> to <div> 
+        }
+    } else {
+        document.getElementById("game_status").textContent += " We ran out words :(";
+    } 
 }
 function restartUI () {
     incorrect_guesses = [];
@@ -29,27 +35,35 @@ function restartUI () {
     document.getElementById("incorrect").textContent = incorrect_guesses;
     createAnswer()
 }
+function winner () {
+    document.getElementById("game_status").textContent = "You Won!";
+}
+function invalidWords(lastWord) {
+    return usedWords.push(lastWord);
+}
 
 createAnswer();
 
 document.onkeyup = function(event) {
-  console.log("You pressed: " + event.key);
-    if (word.indexOf(event.key) > -1) {             // correct guess
-        console.log("you matched on a letter");
+    keyPress = event.key;
+    keyPress = keyPress.toLowerCase();
+  console.log("You pressed: " + keyPress);
+    if (word.indexOf(keyPress) > -1) {             // correct guess
+        // console.log("you matched on a letter");
         var indices = [];
         for (var i = 0; i < word.length; i++) {
-            if (word[i] === event.key) indices.push(i);
-            // answer[indices[i]] = event.key;
+            if (word[i] === keyPress) indices.push(i);
+            // answer[indices[i]] = keyPress;
             // console.log("Answer = " + answer);
         }
-        console.log("Indices match = " + indices);
+        // console.log("Indices match = " + indices);
         for (var j = 0; j < indices.length; j++) {
-            document.getElementById("letter_" + indices[j]).textContent = event.key;
-            answer[indices[j]] = event.key;
-            console.log("Answer = " + answer);
-            console.log(answer.join(""));
+            document.getElementById("letter_" + indices[j]).textContent = keyPress;
+            answer[indices[j]] = keyPress;
+            // console.log("Answer = " + answer);
+            // console.log(answer.join(""));
             if (answer.join("") === word) {
-                alert('You Win!');
+                document.getElementById("game_status").textContent = "You Won!";
                 wins++;
                 document.getElementById('wins').textContent = wins;
                 restartUI();   // Restart the app   
@@ -57,21 +71,20 @@ document.onkeyup = function(event) {
         }
     }
     else {  // incorrect guess
-        if (incorrect_guesses.indexOf(event.key) === -1) {
+        if (incorrect_guesses.indexOf(keyPress) === -1) {
             guesses--;
-            incorrect_guesses.push(event.key);
+            incorrect_guesses.push(keyPress);
             document.getElementById("remaining_guesses").textContent = guesses;
 
             for (var k = 0; k < incorrect_guesses.length; k++) {
-                console.log("Incorrect Guesses: " + incorrect_guesses[k]);
+                // console.log("Incorrect Guesses: " + incorrect_guesses[k]);
                 document.getElementById("incorrect").textContent = incorrect_guesses;
             }
 
         }      
-        if (guesses === 0) {
-            alert ('you lose');
+        if (guesses === 0 || keyPress === '`') {  // pressing '`' key will auto generate loss (Diag only)
+            document.getElementById("game_status").textContent = "You lost :(";
             restartUI();   // Restart the app
-             
         }
     }
 };
