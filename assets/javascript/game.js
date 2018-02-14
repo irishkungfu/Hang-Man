6280f05653baf363579a08812bb34b051b9489e2
Jared Gilpin <irishkungfu@localhost.localdomain>
2018-02-13 22:59:41 -0600

    assignment complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var words = ['gone with the wind', 'casa blanca'];
 var gifs = ['https://thumbs.gfycat.com/HotScalyAztecant-size_restricted.gif','https://thumbs.gfycat.com/ExaltedMeaslyCaiman-size_restricted.gif']
+var songList = ['assets/sounds/gone_with_the_wind.mp3', 'https://ia800305.us.archive.org/21/items/AsTimeGoesBy_591/04Track4.ogg']
 var word;
 var usedWords = []
 var win_gif;
@@ -11,8 +12,27 @@ var wins = 0;
 var img = document.createElement("img");
 var src = document.getElementById("modal_body");
 var item = document.getElementById("modal_body").childNodes[0];
+var songSource;
+var songs = document.createElement('audio')
+songs.id = 'audio';
 
-
+/* Bugs
+    --------------------------------
+    [*]audio only plays the file for the first word selected
+    [ ] content of the document moves when modal pops up
+    [ ] does not provide error messages
+    --------------------------------
+*/
+/* Feature Upgrades
+    --------------------------------
+    * have spaces automatically shown in the UI if they exist
+    * disallow non-relevant keys
+    * press space bar to begin
+    * Use movie tickets? to represent each incorrect guess
+    * Make the background look better
+    * Have the videos play in the cinema area
+    --------------------------------
+*/
 
 
 // Create spans for answer to rest in -- each letter gets a span
@@ -20,10 +40,12 @@ var item = document.getElementById("modal_body").childNodes[0];
 function createAnswer () {
     if (typeof words !== 'undefined' && words.length > 0) {             // Verify words has data in it
         var arrayPosition = Math.floor(Math.random() * words.length);   // Generate a random number that is less than the array length
-        /* i am currently removing one of the words array elements each run through, but not a corresponding
-        gifs array element.  I need to use a different mechanism */
+
         word = words[arrayPosition];                                    // Define the answer word    
         win_gif = gifs[arrayPosition];                                  // Define the correct gif    
+        songSource = songList[arrayPosition];   
+        
+        console.log('songSource = ' + songSource);
 
         console.log('word = ' + word + ' wind_gif = ' + win_gif);
         console.log('-------------------------') 
@@ -32,10 +54,13 @@ function createAnswer () {
         console.log('img.src = ' + img.src );    
         console.log('-------------------------')           
         img.setAttribute("width", "100%")                               // Set image attribute to 100% of container width        
-        src.replaceChild(img, src.childNodes[0]);  
+        src.replaceChild(img, src.childNodes[0]); 
+
 
         words = words.filter(function(a) {return a !== word});          // Remove chosen word from original words array to ensure no repeated words   
         gifs = gifs.filter(function(a) { return a !== win_gif});
+        songList = songList.filter(function (a) { return a !== songSource})
+        console.log('after filter functions, songList is now: ' + songList)
         
         for(var i = 0; i < word.length; i++) {                          // Create a span element for each letter in word                                  
             var node = document.createElement("span");                  
@@ -60,7 +85,15 @@ function startApp () {
 }
 function winner () {                                                    // Overlay Modal and pass in appropriate gif 
     $('#exampleModal').modal();                         // display winning gif
+    console.log('songSource in winner is now equal to: ' + songSource);
+    /* figure out why the audio only works on the first load and not subsequent loads */
+
+    src.appendChild(songs);
+    songs.src = songSource;
+
+    songs.autoplay = false;
     play();
+    setCurTime();
     wins++;
 
     incorrect_guesses = [];                                                 
@@ -82,6 +115,10 @@ function pause(){
     var audio = document.getElementById("audio");
     audio.pause();
 }
+function setCurTime() { 
+    var audio = document.getElementById("audio");
+    audio.currentTime=5;
+} 
 
 /* Run create answer function to generate new answer */
 startApp();
